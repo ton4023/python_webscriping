c674c274e473c51c186f80b9d1c6766154be0335
ton4023 <ton4023@gmail.com>
2021-09-24 13:19:59 +0700

    class tourismthailand

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-DRIVER_PATH = './chromedriver'
-options = Options()
-options.add_argument("--headless")
-
-driver = webdriver.Chrome(executable_path=DRIVER_PATH,options=options)
-driver.get('https://www.tourismthailand.org/Destinations')
-
-tab = driver.find_element_by_class_name('tab-groups')
-tablabel = [i.text for i in tab.find_elements_by_tag_name('li')]
-tabclick = tab.find_elements_by_tag_name('li')
-
-destination = []
-
-for item in tabclick:
-    item.click()
-    topfive = driver.find_element_by_class_name('top-fives-listbox')
-    toplable = [i.text for i in topfive.find_elements_by_tag_name('li')]
-    subDestination = {}
-    subDestination['region'] = item.text
-    subDestination['topFives'] = toplable
-    destination.append(subDestination)
+class TourismThailand:
     
-print(destination)
-driver.close()
+    DRIVER_PATH = './chromedriver'
+    URL_PATH = 'https://www.tourismthailand.org/Destinations'
+    
+    def __init__(self):
+        self.options = Options()
+        self.options.add_argument("--headless")
+        self.driver = webdriver.Chrome(executable_path=self.DRIVER_PATH,options=self.options)
+        self.driver.get(self.URL_PATH)
+    
+    def destination(self):
+        titleDestination = []   
+        tab = self.driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div/div[2]/ul')
+        tabclick =  tab.find_elements_by_tag_name('li')
 
+        for item in tabclick:
+            item.click()
+            topfive = self.driver.find_element_by_class_name('top-fives-listbox')
+            toplable = [i.text for i in topfive.find_elements_by_tag_name('li')]
+            subDestination = {}
+            subDestination['region'] = item.text
+            subDestination['topFives'] = toplable
+            titleDestination.append(subDestination)
+            
+        print(titleDestination)    
+        return titleDestination
+        
+if __name__ == "__main__":
+    tourismThailand = TourismThailand()
+    tourismThailand.destination()
 
-# destination = [
-# 	{
-# 	'region' : 'North',
-# 	'topFives': ['Chiang Mai','Chiang Rai','Mae Hong Son','Lampang','Sukhothai']
-# 	},
-# ]
