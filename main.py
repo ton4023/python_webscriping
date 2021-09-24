fc5a5acf3aff7d63db0dfde6c3b828c2cdfcc5fc
ton4023 <ton4023@gmail.com>
2021-09-24 20:00:34 +0700

    add fastapi

--- main.py
+++ main.py
@@ -1,68 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from fastapi import FastAPI
+from scriping import TourismThailand
+import uvicorn
 
+app = FastAPI()
 
-class TourismThailand:
+@app.get("/")
+def read_root():
+    response = tourismThailand.regionDestination()
+    return {"regionDestination": response}
 
-    DRIVER_PATH = './chromedriver'
-    URL_PATH = 'https://www.tourismthailand.org/Destinations'
-
-    def __init__(self):
-        self.options = Options()
-        self.options.add_argument("--headless")
-        self.driver = webdriver.Chrome(
-            executable_path=self.DRIVER_PATH, options=self.options)
-        self.driver.get(self.URL_PATH)
-
-    def regionDestination(self):
-
-        titleDestination = []
-
-        try:
-            self.driver.implicitly_wait(10)
-            elems = self.driver.find_elements_by_class_name(
-                'tab-groups')[0]
-
-            for item in elems.find_elements_by_tag_name('li'):
-                item.click()
-                topfive = self.driver.find_element_by_class_name(
-                    'top-fives-listbox')
-                toplable = [
-                    i.text for i in topfive.find_elements_by_tag_name('li')]
-                subDestination = {}
-                subDestination['region'] = item.text
-                subDestination['topFives'] = toplable
-                titleDestination.append(subDestination)
-
-            return titleDestination
-
-        finally:
-            self.driver.close()
-
-    def themeDestination(self):
-
-        try:
-            self.driver.implicitly_wait(10)
-
-            themeTravel = []
-
-            elems = self.driver.find_elements_by_class_name('row')[1]
-
-            for i in range(4):
-                place = elems.find_elements_by_class_name('caption')[i]
-                img = elems.find_elements_by_tag_name('img')[i]
-                theme = {}
-                theme['placeName'] = place.text
-                theme['imagePath'] = img.get_attribute('data-src')
-                themeTravel.append(theme)
-
-            return themeTravel
-
-        finally:
-            self.driver.close()
-
+@app.get("/theme")
+def read_theme():
+    response = tourismThailand.themeDestination()
+    return {'themeDestination' : response}
 
 if __name__ == "__main__":
-    tourismThailand = TourismThailand()
-    tourismThailand.regionDestination()
-    tourismThailand.themeDestination()
+    tourismThailand = TourismThailand()  
+    uvicorn.run(app, host="127.0.0.1", port=8000)
